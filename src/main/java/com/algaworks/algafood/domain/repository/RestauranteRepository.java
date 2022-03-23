dcbbc44ae8487512f7e417a54aa6e8142c1fac82
sandrorezende <sandromelandre@gmail.com>
2022-03-23 09:26:07 -0300

    05.09

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,13 +5,20 @@ import java.util.List;
 import java.util.Optional;
 
 import com.algaworks.algafood.domain.model.Restaurante;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
-  List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+  // List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long
+  // cozinha);
+
+  @Query("from Restaurante where nome like %:nome% and cozinha.id =:id")
+  List<Restaurante> ConsultarPorNome(String nome, @Param("id") Long cozinha);
 
   List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
