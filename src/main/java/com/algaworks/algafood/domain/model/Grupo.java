46b65c9a09cf672eb10a5041030a01f5ab756865
sandrorezende <sandromelandre@gmail.com>
2022-08-07 18:09:58 -0300

    "06.8"

---
+++ src/main/java/com/algaworks/algafood/domain/model/Grupo.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Grupo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String grupo;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(name = "grupo_permissoes",
+            joinColumns = @JoinColumn(name = "grupo_id"),
+            inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+    private List<Permissao> permissoes = new ArrayList<>();
+
+}
--- src/main/java/com/algaworks/algafood/domain/model/Produto.java
+++ src/main/java/com/algaworks/algafood/domain/model/Produto.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @Entity
 public class Produto {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
 
     @Column(nullable = false)
